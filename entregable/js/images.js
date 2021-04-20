9468bbf5b6418a2c695480c91f1386412183ff7c
Francoviera <vierafranco1@gmail.com>
2021-04-20 01:50:42 -0300

    se agrego el efecto sepia, contraste y guardar imagen

--- entregable/js/images.js
+++ entregable/js/images.js
@@ -1,22 +1,20 @@
 
-let canvas = document.getElementById("practica");
-let ctx = canvas.getContext('2d');
 let canvas2 = document.getElementById("practicaEdited");
 let ctx2 = canvas2.getContext('2d');
-let canvas3 = document.getElementById("practicaEditedDegrade");
-let ctx3 = canvas3.getContext('2d');
+
 let width = 640;
 let height = 480;
-canvas3.width = 800;
-canvas3.height = 600;
-let imgData= ctx3.createImageData(width, height);
-let imageData = new Image();
 
 let buttonsBlackAndWithe= document.getElementById("aBlancoNegro");
 let buttonInvertirColores= document.getElementById("invertirColores");
 let buttonVolverCambios= document.getElementById("buttonVolverCambios");
 
+buttonsBlackAndWithe.addEventListener("click", () => cambiarBlancoNegro(ctx2));
+buttonInvertirColores.addEventListener("click", () => invertirColores(canvas2, ctx2));
+buttonVolverCambios.addEventListener("click", () => invertirCambios(ctx2));
+
 let inputImage= document.getElementById("inputImage");
+let imageData= null;
 
 inputImage.addEventListener("change", () =>{
     let reader = new FileReader();
@@ -26,29 +24,82 @@ inputImage.addEventListener("change", () =>{
         image.src= reader.result;
         image.onload= () => {
             ctx2.drawImage(image,0,0,image.width,image.height);
-            ctx2.putImageData(ctx2.getImageData,0,0,canvas2.width,canvas2.height);
+            // ctx2.putImageData(ctx2.getImageData(),0,0,canvas2.width,canvas2.height);
+            imageData= ctx2.getImageData(0,0, 400, 600);  //Guardo la data de la foto original 
         }
     }
     reader.readAsDataURL(inputImage.files[0]);
 
-    // borrar imagen inputImage.fila= [];
+});
+
+// borrar imagen inputImage.files= [];
+
+document.getElementById("deleteImage").addEventListener("click", () => {
+    document.getElementById("inputImage").value=""; //elimina el valor del input
+    canvas2.width= canvas2.width; //esto elimina la foto del canvas
 });
 
 
-imageData.src = './images/download.jpg';
+document.getElementById("sepia").addEventListener("click", () =>{
+    invertirCambios();
+    let datosImg= ctx2.getImageData(0,0, 400, 600);
+    let pixels= datosImg.data;
+    numPixels = imageData.width * imageData.height; //saco la cantidad de elementos de la matriz (pixels)
+    for ( var i = 0; i < numPixels; i++ ) {
+        var r = pixels[ i * 4 ];
+        var g = pixels[ i * 4 + 1 ];
+        var b = pixels[ i * 4 + 2 ];
+ 
+        pixels[ i * 4 ] = 255 - r;
+        pixels[ i * 4 + 1 ] = 255 - g;
+        pixels[ i * 4 + 2 ] = 255 - b;
+ 
+        pixels[ i * 4 ] = ( r * .393 ) + ( g *.769 ) + ( b * .189 );
+        pixels[ i * 4 + 1 ] = ( r * .349 ) + ( g *.686 ) + ( b * .168 );
+        pixels[ i * 4 + 2 ] = ( r * .272 ) + ( g *.534 ) + ( b * .131 );
+    }
+ 
+    ctx2.putImageData( datosImg, 0, 0 );
+});
 
-imageData.onload = () => {
-    // myDrawImage(imageData);
-}
+document.getElementById("saturacion").addEventListener("click", () =>{
+    invertirCambios();
+    let imageData = ctx2.getImageData(0,0, 400, 600);
+    let pixels= imageData.data;
+    let numPixels = imageData.width * imageData.height;
+    let contrast = 100; // Default value
+ 
+    let factor = ( 259 * ( contrast + 255 ) ) / ( 255 * ( 259 - contrast ) );
+ 
+    for ( var i = 0; i < numPixels; i++ ) {
+        var r = pixels[ i * 4 ];
+        var g = pixels[ i * 4 + 1 ];
+        var b = pixels[ i * 4 + 2 ];
+ 
+        pixels[ i * 4 ] = factor * ( r - 128 ) + 128;
+        pixels[ i * 4 + 1 ] = factor * ( g - 128 ) + 128;
+        pixels[ i * 4 + 2 ] = factor * ( b - 128 ) + 128;
+    }
+ 
+    ctx2.putImageData( imageData, 0, 0 );
+});
 
-buttonsBlackAndWithe.addEventListener("click", () => cambiarBlancoNegro(canvas2, ctx2));
-buttonInvertirColores.addEventListener("click", () => invertirColores(canvas2, ctx2));
-buttonVolverCambios.addEventListener("click", () => invertirColores(ctx, ctx2));
+document.getElementById("saveImage").addEventListener("click", () =>{
+    var link = window.document.createElement( 'a' ),
+        url = canvas2.toDataURL(),
+        filename = 'screenshot.jpg';
+ 
+    link.setAttribute( 'href', url );
+    link.setAttribute( 'download', filename );
+    link.style.visibility = 'hidden';
+    window.document.body.appendChild( link );
+    link.click();
+    window.document.body.removeChild( link );
+});
 
 
 
 const myDrawImage = (imgData) => {
-        ctx.drawImage(imgData, 0, 0);
         ctx2.drawImage(imgData, 0, 0);
         let datos= ctx2.getImageData(0,0, 400, 600);
         // for (let index = 0; index < 600*400; index++) {
@@ -59,9 +110,8 @@ const myDrawImage = (imgData) => {
         // cambiarBlancoNegro(canvas2, ctx2);
     }
 
-function invertirCambios(ctx, ctx2){
-    let datos= ctx.getImageData(0,0, 400, 600);
-    ctx2.putImageData(datos,0,0);
+function invertirCambios(){
+    ctx2.putImageData(imageData,0,0);
 }
 
 const  dragImgDegrado = (imgData,x,y,r,g,b,a) => {
@@ -72,17 +122,17 @@ const  dragImgDegrado = (imgData,x,y,r,g,b,a) => {
     imgData.data[index + 3] = a;
 }
 
-for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-        dragImgDegrado(imgData,x,y,255+x,0+x,0+x,255+x);
-    }
+// for (let x = 0; x < width; x++) {
+//     for (let y = 0; y < height; y++) {
+//         dragImgDegrado(imgData,x,y,255+x,0+x,0+x,255+x);
+//     }
 
- }
- ctx3.putImageData(imgData, 50, 50);
+//  }
+//  ctx3.putImageData(imgData, 50, 50);
     
 
 function invertirColores(canvas, ctx){
-    console.log("llega")
+    invertirCambios();
     let datosImg= ctx.getImageData(0,0, 400, 600);
     let data= datosImg.data;
     for (let index = 0; index < data.length; index+=4) {
@@ -94,7 +144,8 @@ function invertirColores(canvas, ctx){
     ctx.putImageData(datosImg,0,0);
 }
 
-function cambiarBlancoNegro(canvas, ctx){
+function cambiarBlancoNegro(ctx){
+    invertirCambios();
     let datosImg= ctx.getImageData(0,0, 400, 600);
     let data= datosImg.data;
     for (let index = 0; index < data.length; index+=4) {
