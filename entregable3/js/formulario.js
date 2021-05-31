15a34e5fee4ce56ee34d68df308ad656fdbfc7c7
Francoviera <vierafranco1@gmail.com>
2021-05-31 01:20:22 -0300

    se acomodo el formulario

--- entregable3/js/formulario.js
+++ entregable3/js/formulario.js
@@ -1,10 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (){
+    let checkbox = document.querySelector("input[name=checkbox]");
+    let btns = document.querySelectorAll(".btn-grid");
 
-// const checkbox = document.querySelector('.checkboxVerified');
-// const btns = document.querySelectorAll(".my-form button");
-
-// checkbox.addEventListener("change", function() {
-//   const checked = this.checked;
-//   for (const btn of btns) {
-//     checked ? (btn.disabled = false) : (btn.disabled = true);
-//   }
-// });
+    checkbox.addEventListener("change", function() {
+    let checked = this.checked;
+    for (let btn of btns) {
+        checked ? (btn.disabled = false) : (btn.disabled = true);
+    }
+    });
+});
